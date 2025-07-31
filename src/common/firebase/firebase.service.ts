3930315c50bb83e69c37efe5727fa1d698225229
Milagros <milagrosjulisa.mm@gmail.com>
2025-07-30 19:47:34 -0500

    test: Test ci

--- src/common/firebase/firebase.service.ts
+++ src/common/firebase/firebase.service.ts
@@ -2,6 +2,7 @@ import admin from './firebase-admin';
 
 export class FirebaseService {
   async verifyToken(token: string): Promise<admin.auth.DecodedIdToken> {
+    // Verify the Firebase ID token
     return await admin.auth().verifyIdToken(token);
   }
 }
